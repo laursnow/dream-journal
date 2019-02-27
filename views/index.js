a5a6478ab76f5b8f53d579ad0f92409144ffa719
Lauren Snow <laursnowie@gmail.com>
2019-02-26 23:07:27 -0500

    after mentor session

--- views/index.js
+++ views/index.js
@@ -4,11 +4,47 @@
 
 function watchLogin() {
   $('.js-login-form').on('submit', event => {
-
-    
-
-
+    event.preventDefault();
+    const username = $('#username').val();
+    const pass = $('#pass').val();
+    login(username, pass); 
   });
 }
 
+function watchPost() {
+
+}
+
+function login(user, pass) {
+  $.ajax({
+    method: 'POST',
+    url: '/auth/login',
+    data: JSON.stringify({ 'username': user, 'password' : pass }),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+  })
+    .done(function(res) {
+      sessionStorage.setItem('sessionToken', res.authToken);
+    });
+}
+
+function postEntry(user, pass) {
+  $.ajax({
+    method: 'POST',
+    url: '/entries',
+    data: JSON.stringify({ 'username': user, 'password' : pass }),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    headers: { 'Authorization':  `Bearer ${sessionStorage.getItem('sessionToken')}` }
+  })
+    .done(function(post) {
+      displayOnePost(post);
+    });
+}
+function displayOnePost(post) {
+
+}
+
+
 $(watchLogin);
+
