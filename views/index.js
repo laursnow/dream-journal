30fa0bbf82787b05d2f9d6cba7d0521792e2ce43
Lauren Snow <laursnowie@gmail.com>
2019-02-28 16:50:18 -0500

    logging in works

--- views/index.js
+++ views/index.js
@@ -8,11 +8,9 @@ function watchLogin() {
     const username = $('#username').val();
     const pass = $('#pass').val();
     login(username, pass); 
+    hideLogin();
+    showViewEntries();
   });
-}
-
-function watchPost() {
-
 }
 
 function login(user, pass) {
@@ -22,29 +20,60 @@ function login(user, pass) {
     data: JSON.stringify({ 'username': user, 'password' : pass }),
     contentType: 'application/json; charset=utf-8',
     dataType: 'json',
-  })
-    .done(function(res) {
+    success: function(res) {
       sessionStorage.setItem('sessionToken', res.authToken);
-    });
+    },
+    error: function loginAlert() {
+      alert('Incorrect username or password.');
+    }
+  });
 }
 
-function postEntry(user, pass) {
+function watchPost() {
+  $('.js-post-form').on('submit', event => {
+    event.preventDefault();
+    const title = $('#title').val();
+    const entryNew = $('#dreamentry').val();
+    const entryDate = $('#dreamdate').val();
+    const entryTags = $('#tags').val();
+    console.log(title, entryNew, entryDate, entryTags);
+    postEntry(title, entryNew, entryDate, entryTags); 
+  });
+}
+
+function hideLogin() {
+  $('#login-page').addClass('hidden');
+  $('#h1-login').addClass('hidden');
+};
+
+function showViewEntries() {
+  $('nav').removeClass('hidden');
+  $('#entry-page').removeClass('hidden');
+}
+
+
+function postEntry(title, entryNew, entryDate, entryTags) {
   $.ajax({
     method: 'POST',
     url: '/entries',
-    data: JSON.stringify({ 'username': user, 'password' : pass }),
+    data: JSON.stringify({ 'title': title, 'content': entryNew, 'contentDate': entryDate, 'tags': entryTags }),
     contentType: 'application/json; charset=utf-8',
     dataType: 'json',
     headers: { 'Authorization':  `Bearer ${sessionStorage.getItem('sessionToken')}` }
   })
     .done(function(post) {
-      displayOnePost(post);
+      console.log(post);
     });
 }
-function displayOnePost(post) {
 
-}
+
+// function displayOnePost(post) {
+
+// }
 
 
 $(watchLogin);
+$(watchPost);
+// $(watchUpdate);
+// $(watchDelete)
 
